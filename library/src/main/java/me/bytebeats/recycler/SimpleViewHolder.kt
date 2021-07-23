ee8acbff3cc2135ce118ffc944bc357c23feebd7
panchen <panchen@tigerbrokers.com>
2021-07-23 11:30:07 +0800

    header footer adapter.

---
+++ library/src/main/java/me/bytebeats/recycler/SimpleViewHolder.kt
@@ -0,0 +1,15 @@
+package me.bytebeats.recycler
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * @Author bytebeats
+ * @Email <happychinapc@gmail.com>
+ * @Github https://github.com/bytebeats
+ * @Created on 2021/7/23 10:16
+ * @Version 1.0
+ * @Description TO-DO
+ */
+
+class SimpleViewHolder(val view: View) : RecyclerView.ViewHolder(view)
